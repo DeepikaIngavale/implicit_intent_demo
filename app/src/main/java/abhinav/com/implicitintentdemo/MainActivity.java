214a13ab39ae9242c6916268d60cd5e215ea559f
DeepikaIngavale <deepikaingavale@gmail.com>
2019-02-15 18:39:10 +0530

    DEMO

--- app/src/main/java/abhinav/com/implicitintentdemo/MainActivity.java
+++ app/src/main/java/abhinav/com/implicitintentdemo/MainActivity.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.InputStream;
@@ -21,10 +22,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private static final int IMAGE_CAPTURE=100;
     private static final int RELOAD_IMAGE=101;
-    EditText etxt_email,etxt_phone,etxt_fname,etxt_mname,etxt_lname;
+    private static final int ADD_DATA=102;
+    EditText etxt_email,etxt_phone;
+    TextView txt_fname,txt_mname,txt_lname;
     String email,phone;
-    ImageView imgv_phone,imgv_email,imgv_pick,imgv_gallery,imgv_camera;
-    Button btn_send;
+    ImageView imgv_phone,imgv_email,imgv_pick,imgv_gallery,imgv_camera,imgv_add;
+   // Button btn_send;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,21 +35,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         etxt_email=(EditText)findViewById(R.id.etxt_email);
         etxt_phone=(EditText)findViewById(R.id.etxt_phone);
-        etxt_fname=(EditText)findViewById(R.id.etxt_fname);
-        etxt_mname=(EditText)findViewById(R.id.etxt_mname);
-        etxt_lname=(EditText)findViewById(R.id.etxt_lname);
         imgv_phone=(ImageView) findViewById(R.id.imgv_phone);
         imgv_email=(ImageView) findViewById(R.id.imgv_email);
         imgv_pick=(ImageView) findViewById(R.id.imgv_pick);
         imgv_gallery=(ImageView) findViewById(R.id.imgv_gallery);
         imgv_camera=(ImageView) findViewById(R.id.imgv_camera);
-        btn_send=(Button) findViewById(R.id.btn_send);
+        imgv_add=(ImageView) findViewById(R.id.imgv_add);
+        txt_fname=(TextView)findViewById(R.id.txt_fname);
+        txt_mname=(TextView)findViewById(R.id.txt_mname);
+        txt_lname=(TextView)findViewById(R.id.txt_lname);
+        //btn_send=(Button) findViewById(R.id.btn_send);
 
         imgv_email.setOnClickListener(this);
         imgv_phone.setOnClickListener(this);
         imgv_gallery.setOnClickListener(this);
         imgv_camera.setOnClickListener(this);
-        btn_send.setOnClickListener(this);
+        imgv_add.setOnClickListener(this);
+        //btn_send.setOnClickListener(this);
     }
 
     @Override
@@ -93,14 +98,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
             startActivityForResult(intent, IMAGE_CAPTURE);
         }
-        if(view.getId()==R.id.btn_send)
+
+        if(view.getId()==R.id.imgv_add)
         {
-            Intent intentName=new Intent(MainActivity.this,SetTextActivity.class);
-            intentName.putExtra("Fname",""+etxt_fname.getText().toString().trim());
-            intentName.putExtra("Mname",""+etxt_mname.getText().toString().trim());
-            intentName.putExtra("Lname",""+etxt_lname.getText().toString().trim());
+            Intent intentName=new Intent(this,SetTextActivity.class);
             startActivity(intentName);
         }
+
     }
 
     @Override
@@ -108,35 +112,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (resultCode==RESULT_OK)
+        if(requestCode==RELOAD_IMAGE && resultCode==RESULT_OK)
         {
-
-            switch(requestCode)
+            try
             {
-                case RELOAD_IMAGE:
-                    try
-                    {
-                        Uri selectedImage=data.getData();
-                        Bitmap selected_img_bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImage);
-                        imgv_pick.setImageBitmap(selected_img_bitmap);
+                Uri selectedImage=data.getData();
+                Bitmap selected_img_bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImage);
+                imgv_pick.setImageBitmap(selected_img_bitmap);
 
-                    }
-                    catch (Exception e)
-                    {
-                        e.printStackTrace();
-                    }
-                    break;
-                case IMAGE_CAPTURE :
-
-                    Bundle extras = data.getExtras();
-                    Bitmap imageBitmap = (Bitmap) extras.get("data");
-
-                    imgv_pick.setImageBitmap(imageBitmap);
-                    break;
             }
-
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
         }
+        if(requestCode==IMAGE_CAPTURE && resultCode==RESULT_OK)
+        {
+                Bundle extras = data.getExtras();
+                Bitmap imageBitmap = (Bitmap) extras.get("data");
 
+                imgv_pick.setImageBitmap(imageBitmap);
+        }
+        if(resultCode==RESULT_OK)
+        {
+            if(requestCode==ADD_DATA)
+            {
+                String name = getIntent().getStringExtra("Fname");
+                String mname = getIntent().getStringExtra("Mname");
+                String lname = getIntent().getStringExtra("Lname");
+                /*txt_fname.setText(name);
+                txt_mname.setText(mname);
+                txt_lname.setText(lname);*/
+                Toast.makeText(this, ""+name +" "+mname+" "+lname, Toast.LENGTH_SHORT).show();
+            }
+        }
     }
    protected void openDialer()
     {
--- app/src/main/java/abhinav/com/implicitintentdemo/SetTextActivity.java
+++ app/src/main/java/abhinav/com/implicitintentdemo/SetTextActivity.java
@@ -3,28 +3,34 @@ package abhinav.com.implicitintentdemo;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class SetTextActivity extends AppCompatActivity {
-    TextView txt_fname,txt_mname,txt_lname;
+    //private static final int RESULT_OK=1;
+    EditText etxt_fname,etxt_mname,etxt_lname;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_text);
 
-        txt_fname=(TextView)findViewById(R.id.txt_fname);
-        txt_mname=(TextView)findViewById(R.id.txt_mname);
-        txt_lname=(TextView)findViewById(R.id.txt_lname);
+        etxt_fname=(EditText)findViewById(R.id.etxt_fname);
+        etxt_mname=(EditText)findViewById(R.id.etxt_mname);
+        etxt_lname=(EditText)findViewById(R.id.etxt_lname);
 
-        Intent intent=getIntent();
-        String name=intent.getStringExtra("Fname");
-        String mname=intent.getStringExtra("Mname");
-        String lname=intent.getStringExtra("Lname");
-        txt_fname.setText(name);
-        txt_mname.setText(mname);
-        txt_lname.setText(lname);
-        Toast.makeText(this, ""+name +" "+mname+" "+lname, Toast.LENGTH_SHORT).show();
+       //onBackPressed();
     }
+    /*public void onBackPressed()
+    {
+        super.onBackPressed();
+
+        Intent intent = new Intent();
+        intent.putExtra("Fname", ""+etxt_fname.getText().toString().trim());
+        intent.putExtra("Mname", ""+etxt_mname.getText().toString().trim());
+        intent.putExtra("Lname", ""+etxt_lname.getText().toString().trim());
+        setResult(RESULT_OK, intent);
+        finish();
+    }*/
 }
